5e2f7dbfbe9a9ae8a40838ef9b32b6a565efd84c
Arman Aydemir <armana22@gmail.com>
2018-08-21 04:59:02 -0400

    adding request on starting vc

--- Scroll Data/StartingViewController.swift
+++ Scroll Data/StartingViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class StartingViewController: UIViewController {
+    var articles: Array<Any> = []
+    var titles: Array<String> = []
     @IBOutlet weak var articleLink: UITextField!
     @IBOutlet weak var loadIndicator: UIActivityIndicatorView!
     
-    @IBOutlet weak var articles: UITableView!
+
+    @IBOutlet weak var table: UITableView!
     @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.articleLink.text = "https://www.nytimes.com/2018/08/13/world/europe/erdogan-turkey-lira-crisis.html"
-        //self.startButton.setTitle("Tap tp Start Reading", for: UIControlState.normal)
+        self.articleLink.text = "https://www.nytimes.com/2018/08/13/world/europe/erdogan-turkey-lira-crisis.html"
+        self.startButton.setTitle("Tap tp Start Reading", for: UIControlState.normal)
         // Do any additional setup after loading the view.
+        guard let table = self.table, let loadIndicator = self.loadIndicator, let startButton = self.startButton, let articleLink = self.articleLink else {
+            print("couldn't connect starting vc outlets! bad things coming.....")
+            return
+        }
+        table.isHidden = true;
+        loadIndicator.hidesWhenStopped = true
+        loadIndicator.startAnimating()
+        
+        Networking.request(headers:nil, method: "GET", fullEndpoint: "http://159.203.207.54:22364/articles", body: nil, completion: { data, response, error in
+            if let dataExists = data, error == nil {
+                do {
+                    if let articles = try JSONSerialization.jsonObject(with: dataExists, options: .allowFragments) as? Array<Any> {
+                        print(articles)
+                    
+                    } else {
+                        throw NSError(domain: "invalid json", code: 1, userInfo: nil)
+                    }
+                }catch let err{
+                    print("invalidddd")
+                }
+            }else{
+                print("server disconnect, gotta do somethin here")
+                //self.text[0] = "problem connecting to server";
+            }
+            
+            DispatchQueue.main.async{
+                loadIndicator.stopAnimating()
+                table.reloadData()
+                
+                
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,12 +70,12 @@ class StartingViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let vc = segue.destination
-//        if let destination:ArticleViewController = vc as? ArticleViewController {
-//            destination.articleLink = self.articleLink.text;
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination
+        if let destination:ArticleViewController = vc as? ArticleViewController {
+            destination.articleLink = self.articleLink.text;
+        }
+    }
     
 
 }
