bdf2f8d4bdea0d2b16e6aa49e1b29fc30c6e1333
Arman Aydemir <armana22@gmail.com>
2018-11-26 23:35:59 -0700

    moving some stuff around, running well

--- Scroll Data/ArticleViewController.swift
+++ Scroll Data/ArticleViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-@objc class ArticleViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate, SubmitTableViewCellDelegate {
+@objc class ArticleViewController: UIViewController {
     
     var content: Array<Content> = []
     var articleLink: String?
@@ -56,8 +56,6 @@ import UIKit
         spinner.hidesWhenStopped = true
         spinner.startAnimating()
         
-        
-        
 //        let model = UIDevice.current.model
 //        if(model == "iPad"){
 //            //            NSLayoutConstraint.activate([
@@ -87,8 +85,8 @@ import UIKit
                 return
             }
             DispatchQueue.main.async {
-                let font = self.findFontSize(table: table) ?? UIFont.preferredFont(forTextStyle: .body)
-                self.content = self.convert(paragraphs: p, font: font)
+                self.font = self.findFontSize(table: table) ?? UIFont.preferredFont(forTextStyle: .body)
+                self.content = self.convert(paragraphs: p, font: self.font)
             
                 table.reloadData()
                 
@@ -116,16 +114,86 @@ import UIKit
         return SystemFont.init(fontName: "Times New Roman")?.fontToFit(text: string, inSize: size, spacing: ArticleTextTableViewCell.topSpacingConstant*2)
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.row {
-        case 0:
-            return UIScreen.main.bounds.size.height - (self.navigationController?.navigationBar.frame.size.height ?? 0) - 1
-        case (self.content.count-1):
-             return UIScreen.main.bounds.size.height - (self.navigationController?.navigationBar.frame.size.height ?? 0) - 1
-        default:
-            return UITableViewAutomaticDimension
-        }
+    func currentPosition(tableView:UITableView) -> Array<Int> {
+
+        let first_index = tableView.indexPath(for: tableView.visibleCells.first!)?.item
+        let second_index = tableView.indexPath(for: tableView.visibleCells.last!)?.item
+        
+        return [first_index!, second_index!]
     }
+    
+    func sendTextToServer(tableView:UITableView) -> Void {
+        //print("send text - \(CFAbsoluteTimeGetCurrent())")
+        let current_offset = tableView.contentOffset.y
+        if tableView.isHidden || current_offset == self.content_offset { return }
+        
+        let temp = currentPosition(tableView: tableView)
+        let first_index = temp[0], last_index = temp[1]
+            
+        vm.submitData(content_offset: current_offset, first_index: first_index, last_index: last_index)
+        
+    }
+    
+    func cellFit(string:String, attributes:[String: Any]) -> Bool {
+        guard let checker = self.minTableDim else {
+            //print("not table exists, this should never happen")
+            print("error in cellFit")
+            return false
+        }
+        
+        return (string as NSString).size(attributes: attributes).width < checker - ArticleTextTableViewCell.widthSpacingConstant*2
+    }
+    
+
+    
+    @objc func willResignActive(_ notification: Notification) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func convert(paragraphs: [String], font: UIFont) -> [Content] {
+        var cells = [Content].init()
+        var p = 0
+        var word_count = 0
+        var char_count = 0
+        let attributes = [NSFontAttributeName: font] as [String : Any]
+        cells.append(Line.init(text: paragraphs[0], paragraph: p, firstWordIndex: word_count, firstCharacterIndex: char_count))
+        p+=1
+        for section in paragraphs.dropFirst(){
+            var words = section.split(separator: " ").map({substring in
+                return String.init(substring)
+            })
+            
+            while(!words.isEmpty){
+                var cell = [String]()
+                var temp = [words[0]]
+                while(!words.isEmpty && cellFit(string: temp.joined(separator: " "), attributes: attributes)){
+                    cell.append(words.removeFirst())
+                    if let w = words.first{
+                        temp = cell
+                        temp.append(w)
+                    }
+                }
+                word_count += cell.count
+                let current_text = cell.joined(separator: " ")
+                char_count += current_text.count
+                cells.append(Line.init(text: current_text, paragraph: p, firstWordIndex: word_count, firstCharacterIndex: char_count))
+
+            }
+            p += 1
+            cells.append(Spacer())
+        }
+        return cells
+    }
+}
+
+extension ArticleViewController: SubmitTableViewCellDelegate {
+    func submitData() {
+        self.complete = true
+        _ = navigationController?.popViewController(animated: true)
+    }
+}
+
+extension ArticleViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (self.content.count > 0) ? self.content.count + 1 : 0 //plus one for submit button
@@ -167,98 +235,15 @@ import UIKit
         }
     }
     
-    func submitData() {
-        self.complete = true
-        _ = navigationController?.popViewController(animated: true)
-    }
-    
-    func currentPosition(tableView:UITableView) -> Array<Int> {
-
-        let first_index = tableView.indexPath(for: tableView.visibleCells.first!)?.item
-        let second_index = tableView.indexPath(for: tableView.visibleCells.last!)?.item
-        
-        return [first_index!, second_index!]
-    }
-    
-    func sendTextToServer(tableView:UITableView) -> Void {
-        //print("send text - \(CFAbsoluteTimeGetCurrent())")
-        let current_offset = tableView.contentOffset.y
-        if tableView.isHidden || current_offset == self.content_offset { return }
-        
-        let temp = currentPosition(tableView: tableView)
-        let first_index = temp[0], last_index = temp[1]
-            
-        vm.submitData(content_offset: current_offset, first_index: first_index, last_index: last_index)
-        
-    }
-    
-    func cellFit(string:String, attributes:[String: Any]) -> Bool {
-        guard let checker = self.minTableDim else {
-            //print("not table exists, this should never happen")
-            print("error in cellFit")
-            return false
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0:
+            return UIScreen.main.bounds.size.height - (self.navigationController?.navigationBar.frame.size.height ?? 0) - 1
+        case (self.content.count-1):
+            return UIScreen.main.bounds.size.height - (self.navigationController?.navigationBar.frame.size.height ?? 0) - 1
+        default:
+            return UITableViewAutomaticDimension
         }
-        
-        return (string as NSString).size(attributes: attributes).width < checker - ArticleTextTableViewCell.widthSpacingConstant*2
-    }
-    
-
-    
-    @objc func willResignActive(_ notification: Notification) {
-        _ = navigationController?.popViewController(animated: true)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let characterIndices = self.content.flatMap {
-                return $0 as? Line
-            } .map {
-                return $0.firstCharacterIndex
-        }
-        
-        let wordIndices = self.content.flatMap {
-            return $0 as? Line
-            } .map {
-                return $0.firstWordIndex
-        }
-        
-        vm.closeArticle(wordIndicies: wordIndices, characterIndicies: characterIndices, complete: self.complete)
-    }
-    
-    func convert(paragraphs: [String], font: UIFont) -> [Content] {
-        var cells = [Content].init()
-        var p = 0
-        var word_count = 0
-        var char_count = 0
-        let attributes = [NSFontAttributeName: font] as [String : Any]
-        cells.append(Line.init(text: paragraphs[0], paragraph: p, firstWordIndex: word_count, firstCharacterIndex: char_count))
-        p+=1
-        for section in paragraphs.dropFirst(){
-            var words = section.split(separator: " ").map({substring in
-                return String.init(substring)
-            })
-            
-            while(!words.isEmpty){
-                var cell = [String]()
-                var temp = [words[0]]
-                while(!words.isEmpty && cellFit(string: temp.joined(separator: " "), attributes: attributes)){
-                    cell.append(words.removeFirst())
-                    if let w = words.first{
-                        temp = cell
-                        temp.append(w)
-                    }
-                }
-                word_count += cell.count
-                let current_text = cell.joined(separator: " ")
-                char_count += current_text.count
-                cells.append(Line.init(text: current_text, paragraph: p, firstWordIndex: word_count, firstCharacterIndex: char_count))
-
-            }
-            p += 1
-            cells.append(Spacer())
-        }
-        return cells
     }
 }
 
@@ -292,10 +277,6 @@ struct Spacer: Content {
         return ""
     }
 }
-
-
-
-
 
 let sizingString = """
 President Trump’s $1.5 trillion tax cut was supposed to be a big selling point for congressional Republicans in the midterm elections. Instead, it appears to have done more to hurt, than help, Republicans in high-tax districts across California, New Jersey, Virginia and other states.
