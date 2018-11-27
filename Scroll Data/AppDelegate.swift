4d598400e4d77868d2babdba59c9223d01fcfe87
Arman Aydemir <armana22@gmail.com>
2018-11-26 18:26:10 -0700

    switched app delegate to swift

---
+++ Scroll Data/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Scroll Data
+//
+//  Created by Aydemir on 11/26/18.
+//  Copyright © 2018 Arman Aydemir. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+}
